f513efed44f7a6f689f9b48e05c3416aae550747
AmedeoPasanisi <amedeopasanisi97@gmail.com>
2022-03-23 16:38:20 +0100

    addEventListener click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 var element = document.getElementById('secondaParola');
 
 element.style.color = 'red';
+
+element.addEventListener('click',
+    function() {
+        if (element.style.color == 'red') {
+            element.style.color = 'blue';
+        } else {
+            element.style.color = 'red';
+        }
+    }
+)
