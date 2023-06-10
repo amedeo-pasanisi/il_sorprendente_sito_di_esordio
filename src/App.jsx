0b064489fea629f0fe619058274cf5fd85bee54b
Utente <Utente@email.com>
2023-06-10 19:51:04 +0200

    dev branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
