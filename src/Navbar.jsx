023525e1219fd91b6777d9c8d88fe7d27f21af3a
Utente <Utente@email.com>
2023-06-17 22:20:18 +0200

    super market link fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <Link to="https://super-market-three.vercel.app/" target="_blank"><li>Super Mercato</li></Link>
+                <Link to="https://supermarkettest.vercel.app/" target="_blank"><li>Super Mercato</li></Link>
                 <Link to ="https://three-js-tests-amedeo-pasanisi.vercel.app/" target="_blank"><li>Elis Innovation Hub Logo</li></Link>
                 <Link to="https://fox-animation-zeta.vercel.app/" target="_blank"><li>Volpe</li></Link>
                 <Link to="https://camera-positions-data.vercel.app/" target="_blank"><li>Automobile</li></Link>
