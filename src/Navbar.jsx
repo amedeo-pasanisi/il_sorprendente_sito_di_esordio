f7cb9ba95a3d2ad11c13981b035088d97841627a
Utente <Utente@email.com>
2023-06-17 18:34:14 +0200

    purpose of this site change: portfolio

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <>
+        <nav className="navbar">
             <ul>
-                <Link to ="https://three-js-tests-amedeo-pasanisi.vercel.app/" target="_blank"><li>EIH Logo</li></Link>
+                <Link to="https://super-market-three.vercel.app/" target="_blank"><li>Super Mercato</li></Link>
+                <Link to ="https://three-js-tests-amedeo-pasanisi.vercel.app/" target="_blank"><li>Elis Innovation Hub Logo</li></Link>
                 <Link to="https://fox-animation-zeta.vercel.app/" target="_blank"><li>Volpe</li></Link>
                 <Link to="https://camera-positions-data.vercel.app/" target="_blank"><li>Automobile</li></Link>
             </ul>
-        </>
+        </nav>
     )
 }
