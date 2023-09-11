3f598a2cded252d70be0ccdf73ceddbfb16f8080
Amedeo Pasanisi <a.pasanisi@elis.org>
2023-09-11 18:01:35 +0200

    video game link added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,11 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <Link to="https://supermarkettest.vercel.app/" target="_blank"><li>Super Mercato</li></Link>
-                <Link to ="https://three-js-tests-amedeo-pasanisi.vercel.app/" target="_blank"><li>Elis Innovation Hub Logo</li></Link>
-                <Link to="https://fox-animation-zeta.vercel.app/" target="_blank"><li>Volpe</li></Link>
-                <Link to="https://camera-positions-data.vercel.app/" target="_blank"><li>Automobile</li></Link>
+                <Link to="https://supermarkettest.vercel.app/" target="_blank"><li>Super Mercato (React)</li></Link>
+                <Link to="https://react-three-fiber-video-game.vercel.app/" target="_blank"><li>Video Game (React Three Fiber)</li></Link>
+                <Link to ="https://three-js-tests-amedeo-pasanisi.vercel.app/" target="_blank"><li>Elis Innovation Hub Logo (Three.js)</li></Link>
+                <Link to="https://fox-animation-zeta.vercel.app/" target="_blank"><li>Volpe (Three.js)</li></Link>
+                <Link to="https://camera-positions-data.vercel.app/" target="_blank"><li>Automobile (Three.js)</li></Link>
             </ul>
         </nav>
     )
