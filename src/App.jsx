6d5863d68a164dc22d82edf694d6e93ae17ec5ee
Amedeo Pasanisi <a.pasanisi@elis.org>
2024-06-17 17:04:11 +0200

    added heart texture

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
 
 function App() {
 
-  return (
+  return (<>
+    <div className='htmlContainer'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
     <Canvas
       eventPrefix="client"
       eventSource={document.getElementById('root')}
     >
       <Experience />
     </Canvas>
+  </>
   )
 }
 
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,11 @@
-import {useRef} from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import { useRef } from 'react'
 import * as THREE from 'three'
-import {useFrame} from '@react-three/fiber'
-import {BallCollider, Physics, RigidBody} from '@react-three/rapier'
-import {Html} from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
+import { useTexture } from '@react-three/drei'
 
 export default function Experience() {
+    const texture = useTexture({map:'heart_day_color_map.jpg', roughnessMap: 'heart_specular_map.jpg'})
     const api = useRef()
     const vec = new THREE.Vector3()
     useFrame(() => {
@@ -17,27 +16,18 @@ export default function Experience() {
     })
     
     return <>
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[1, 1, 1]} intensity={5}/>
         <Physics timeStep="vary" gravity={[0, 0, 0]}>
             <Pointer />
             <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={[1, 0, 1]} ref={api} colliders={false}>
                 <BallCollider args={[0.5]} />
-                <mesh  castShadow receiveShadow>
+                <mesh castShadow receiveShadow>
                     <sphereGeometry args={[0.5, 64, 64]} />
-                    <meshNormalMaterial />
+                    <meshStandardMaterial {...texture} />
                 </mesh>
             </RigidBody>
         </Physics>
-        <Html
-            transform
-            distanceFactor={4}
-            occlude='blending'
-        >
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </Html>
     </>
 }
 
