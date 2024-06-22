1355855fc04fd2aa768cb72b12c9ede93edcef01
Amedeo Pasanisi <a.pasanisi@elis.org>
2024-06-22 19:16:58 +0200

    pointer spawn on first user pointer move

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,23 @@
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
 import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
 import { useTexture } from '@react-three/drei'
 
 export default function Experience() {
+    const [isPointerMoved, setIsPointerMoved] = useState(false)
     const texture = useTexture({map:'heart_day_color_map.jpg', roughnessMap: 'heart_specular_map.jpg'})
     const api = useRef()
     const vec = new THREE.Vector3()
+
+    const handleMouseMove = () => {
+        setIsPointerMoved(true)
+        window.removeEventListener('pointermove', handleMouseMove)
+    }
+    useEffect(() => {
+        window.addEventListener('pointermove', handleMouseMove)
+    }, [])
+
     useFrame(() => {
         api.current?.
             applyImpulse(vec.copy(api.current.translation()).
@@ -19,7 +29,7 @@ export default function Experience() {
         <ambientLight intensity={1.5} />
         <directionalLight position={[1, 1, 1]} intensity={5}/>
         <Physics timeStep="vary" gravity={[0, 0, 0]}>
-            <Pointer />
+            {isPointerMoved && <Pointer />}
             <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={[1, 0, 1]} ref={api} colliders={false}>
                 <BallCollider args={[0.5]} />
                 <mesh castShadow receiveShadow>
@@ -41,7 +51,7 @@ function Pointer({ vec = new THREE.Vector3() }) {
     })
     
     return (
-        <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={ref}>
+        <RigidBody position={[20,20,20]} type="kinematicPosition" colliders={false} ref={ref}>
             <BallCollider args={[0.1]} />
         </RigidBody>
     )
